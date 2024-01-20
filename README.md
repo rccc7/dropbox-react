c7340911fe847f8428b21ae13a7b7f85686ce18f
RCCC <rcristhiancc@gmail.com>
2024-01-20 04:15:06 -0400

    Some modifications to page.tsx: added more descriptions and changed dropbox with dropbex in some texsts. Added an screenshot to show it in the Readme.md file. Updated the Readme.md file

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+# Trello - React.
+
+A Dropbox-like app that simulates Dropbox UI and functionality. Users can securely login using their Google account or sing up using their email (powered by [Clerk](https://clerk.com/)) and then upload, download, rename and delete their files simulating the real Dropbox functionality. Files are hosted in Firebase storage, and the details and association with the users are registered in the Firestore Database.
+
+See it: https://dropbox-react.vercel.app/
+
+## Screenshots:
+
+<div align="center">
+  <img src="screenshots/Dropbox-React.jpg" alt="screenshot" width="700" style="width:700px;"/>
+</div>
+
+## Features.
+
+- Drag and Drop functionality. The user can upload their files either by opening a File dialog or by dragging files to the app's drop zone. Powered by [react-dropzone](https://react-dropzone.js.org/), a simple react hook to create an HTML 5 compilant drag'n drop zone for files.
+- Data persistence. Upload files and store them in the cloud through Firebase storage. You can add, rename, delete your personal files.
+- User management. Users can Sign in securely using their Google account or email. Powered by [Clerk](https://clerk.com/). Each user can access only their own files.
+- State management powered by [Zustand](https://www.npmjs.com/package/zustand).
+- Dark mode functionality as well as beautiful reusable components such as button, dialog, dropdown mnenu, table and input powered by [shacdn/ui](https://ui.shadcn.com/)
+- Search functionality. Search for a task and filter the todos lists in realtime as you type in the search box.
+
+## Technologies:
+
+The following technologies, dependencies, and services were used to make this app:
+
+- ReactJS.
+- NextJS 14.
+- TailwindCSS.
+- Shacdn/ui: Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.
+- [AppWrite Cloud](https://cloud.appwrite.io/). A secure open-source backend platform which provides a set of secure APIS, tools and a management console UI to help build apps quickly. Features used: database and file storage.
+- [React-Beautiful-DND](https://github.com/atlassian/react-beautiful-dnd). A beautiful and accessible drag and drop library for lists with React.
+- [Zustand](https://www.npmjs.com/package/zustand). A small, fast and sacalable bearbones state-management solution using simplified flux principles.
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       {/* <h1 className="text-red-500 dark:text-blue-500">Dropbex React</h1> */}
       <div className="flex flex-col lg:flex-row items-center bg-[#2B2929] dark:bg-slate-800 ">
         <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 space-y-5 text-white">
-          <h1 className="text-5xl font-bold">
-            Welcome to Dropbex. <br />
+          <h1 className="text-4xl font-bold">
+            Welcome to Dropbox-React. <br />
             <br />
             Storing everything for you and your business needs. All in one
             place.
@@ -20,13 +20,26 @@ export default function Home() {
             store important documents and media, and experience the convenience
             of easy file management and sharing in one centralized solution.
           </p>
-          <Link
-            className="flex cursor-pointer bg-blue-500 p-5 w-fit"
-            href={"/dashboard"}
-          >
-            Try it for free!
-            <ArrowRight className="ml-10" />
-          </Link>
+          <div className="flex flex-row justify-start gap-x-2 items-center">
+            <Link
+              className="flex cursor-pointer bg-blue-500 p-5 w-fit"
+              href={"/dashboard"}
+            >
+              Try it for free!
+              <ArrowRight className="ml-10" />
+            </Link>
+            <h1 className="text-xs w-44">
+              Sign in securely using your Google account or email.{" "}
+              <a
+                href="https://clerk.com/"
+                target="_blank"
+                className="text-blue-500 underline"
+              >
+                Secured by Clerk
+              </a>
+              .
+            </h1>
+          </div>
         </div>
         <div className="flex flex-col bg-[#1E1919] dark:bg-slate-800 p-10 h-full sjustify-center">
           <video autoPlay loop muted className="rounded-lg">
@@ -38,13 +51,31 @@ export default function Home() {
         </div>
       </div>
       <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
-      <p className="text-center font-light p-2">
+      <p className="text-center font-light text-sm p-2">
         This app was made for informational, educational, and testing purposes
         only. There is no commercial intention. The main goal of this app is to
         practice and demonstrate the latest features of Next.js. I do not own or
         affiliate with Dropbox or/and any of its subsidiaries in any form.
         Copyright disclaimer under section of the Copyright Act 1976, allowance
-        is made for &quot;fair use&quot;
+        is made for &quot;fair use&quot;.{" "}
+      </p>
+      <p className="text-center text-sm font-light p-2">
+        <span className="font-bold">Important notice:</span> When you log in, we
+        do not get personal information except of your user name, name, and
+        email for a personalized UI interaction.
+      </p>
+      <p className="text-center text-sm">
+        Dropbox logo obtained from Wikimedia Commons and video from{" "}
+        <a
+        // className="text-blue-500 underline"
+        // href="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+        >
+          Dropbox site.
+        </a>
+      </p>
+      <p className="text-center text-sm p-2">
+        THIS IS NOT THE OFFICIAL DROPBOX. THIS IS A REACTJS DEMO APP FOR TESTING
+        AND DEMONSTRATION PURPOSES ONLY.
       </p>
     </main>
   );
